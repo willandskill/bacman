edffb7e6adb5e4ea58091c55f370f355bd07871a
Faisal Mahmud <faisal@willandskill.se>
2015-10-04 23:11:37 +0200

    Renamed files

--- bacman/mysql.py
+++ bacman/mysql.py
@@ -1,24 +1,22 @@
 import os
 
-from bacman import BacMan
+from .bacman import BacMan
 
 
-class Postgres(BacMan):
-    """ Take a snapshot of a Postgres DB. """
+class MySQL(BacMan):
+    """Take a snapshot of a MySQL DB."""
 
-    filename_prefix = os.environ.get('BACMAN_PREFIX', 'pgdump')
-    suffix = "bak"
+    filename_prefix = os.environ.get('BACMAN_PREFIX', 'mysqldump')
 
     def get_command(self, path):
-        command_string = "export PGPASSWORD={password}\npg_dump -Fc -U {user} -h {host} -p {port} {name} -f {path}"
+        command_string = "mysqldump -u {user} -p{password} -h {host} {name} > {path}"
         command = command_string.format(user=self.user,
                                         password=self.password,
                                         host=self.host,
-                                        port=self.port,
                                         name=self.name,
                                         path=path)
         return command
 
 
 if __name__ == "__main__":
-    Postgres()
+    MySQL()
--- bacman/postgres.py
+++ bacman/postgres.py
@@ -1,22 +1,24 @@
 import os
 
-from bacman import BacMan
+from .bacman import BacMan
 
 
-class MySQL(BacMan):
-    """Take a snapshot of a MySQL DB."""
+class Postgres(BacMan):
+    """ Take a snapshot of a Postgres DB. """
 
-    filename_prefix = os.environ.get('BACMAN_PREFIX', 'mysqldump')
+    filename_prefix = os.environ.get('BACMAN_PREFIX', 'pgdump')
+    suffix = "bak"
 
     def get_command(self, path):
-        command_string = "mysqldump -u {user} -p{password} -h {host} {name} > {path}"
+        command_string = "export PGPASSWORD={password}\npg_dump -Fc -U {user} -h {host} -p {port} {name} -f {path}"
         command = command_string.format(user=self.user,
                                         password=self.password,
                                         host=self.host,
+                                        port=self.port,
                                         name=self.name,
                                         path=path)
         return command
 
 
 if __name__ == "__main__":
-    MySQL()
+    Postgres()
