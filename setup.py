29591a52f511e22d82f6b1bb12b1772d8d0e5a21
Faisal Mahmud <faisal@willandskill.se>
2019-09-18 23:45:43 +0200

    Update setup.py to load text from README.md

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+# Read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='bacman',
     version='0.3',
@@ -13,7 +19,8 @@ setup(
     license='BSD licence, see LICENCE.txt',
     description='Easily take snapshots of Postgres and MySQL databases and upload to AWS S3.',
     keywords=['backup', 'database', 'mysql', 'postgres'],
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     zip_safe=False,
     install_requires=[
         "dj-database-url>=0.3.0",
